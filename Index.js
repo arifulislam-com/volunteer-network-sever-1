257d7706e432d7871fed0bbe87789f43b45d32e0
arifulislam-com <arifulislam9565@gmail.com>
2021-06-24 14:49:03 +0600

    somthing change

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
-const port = 5000
+const port = 6000
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifnyo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
